59bc1f2bfbd014dfd3dcb336347a24d3d85e3f25
soudyjr <elsoudy222@gmail.com>
2024-01-26 11:06:44 +0400

    add splash structure

---
+++ lib/view/splash/widgets/splash_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class SplashScreenBody extends StatelessWidget {
+  const SplashScreenBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column();
+  }
+}
