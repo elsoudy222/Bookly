f424bda4a3dcab7eacb06381b706140f926b3d42
soudyjr <elsoudy222@gmail.com>
2024-01-28 22:48:41 +0400

    add font family

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'core/utils/const/colors/colors.dart';
 import 'features/splash/splash_screen.dart';
@@ -33,7 +34,11 @@ class MyApp extends StatelessWidget {
         theme: ThemeData.dark().copyWith(
           scaffoldBackgroundColor: AppColors.scaffoldBackgroundColor,
           useMaterial3: true,
+          textTheme: GoogleFonts.montserratTextTheme(
+            ThemeData.dark().textTheme,
+          ),
         ),
+
         home: const SplashScreen(),
       ),
 
