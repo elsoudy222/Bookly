81c0c028456c3f3fff3e09b762ad1a88b4a1f52b
soudyjr <elsoudy222@gmail.com>
2024-01-28 23:01:26 +0400

    Best Seller List View Item Part 1

--- lib/features/home/presentation/view/widgets/home_view_body.dart
+++ lib/features/home/presentation/view/widgets/home_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../../../../core/utils/const/assets_path/assets_path.dart';
 import 'custom_app_bar.dart';
 import 'featured_list_view.dart';
 
@@ -9,23 +10,59 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Column(
+    return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         const CustomAppBar(),
         const FeaturedBooksListView(),
-        SizedBox(height: 35.h,),
+        SizedBox(
+          height: 35.h,
+        ),
         Padding(
           padding: EdgeInsets.symmetric(horizontal: 13.0.r),
-          child: const Text(
-            "Best Seller",
-            style: Styles.textStyle20,
+          child: const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Best Seller",
+                style: Styles.textStyle20,
+              ),
+              BestSellerListViewItem(),
+            ],
           ),
-        )
+        ),
+
       ],
     );
   }
 }
 
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Row(
+        children: [
+          Container(
+            height: 100.h,
+            width: 70.w,
+            decoration:  BoxDecoration(
+              color: Colors.orange,
+              borderRadius: BorderRadius.circular(15),
+              image: const DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage(
+                  AssetsPaths.testPic,
 
+                ),
+
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
