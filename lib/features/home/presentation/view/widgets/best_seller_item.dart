e534c169e9a0979fcb7c53bfc8eb24ce6f90fb3d
soudyjr <elsoudy222@gmail.com>
2024-01-28 23:57:34 +0400

    Create Book Rating and finish best seller item

--- lib/features/home/presentation/view/widgets/best_seller_item.dart
+++ lib/features/home/presentation/view/widgets/best_seller_item.dart
@@ -4,6 +4,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../../../../core/utils/const/assets_path/assets_path.dart';
 import '../../../../../core/utils/const/const.dart';
 import '../../../../../core/utils/styles.dart';
+import 'book_rating.dart';
 
 
 
@@ -34,48 +35,49 @@ class BestSellerListViewItem extends StatelessWidget {
           const SizedBox(
             width: 30,
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width * .5,
-                child: Text(
-                  "Harry Potter and the Goblet of Fire",
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  style: Styles.textStyle20.copyWith(
-                   // fontFamily: kGtSectraFine,
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 3,
-              ),
-              const Text(
-                "J.K. Rowling",
-                style: Styles.textStyle14,
-              ),
-              const SizedBox(
-                height: 3,
-              ),
-              Row(
-                children: [
-                  Text(
-                    'Free',
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * .5,
+                  child: Text(
+                    "Harry Potter and the Goblet of Fire",
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
                     style: Styles.textStyle20.copyWith(
-                      fontWeight: FontWeight.bold,
+                     // fontFamily: kGtSectraFine,
                     ),
                   ),
-                  //      const Spacer(),
-                  // BookRating(
-                  //   rating:
-                  //   bookModel.volumeInfo.averageRating?.round() ?? 0,
-                  //   count: bookModel.volumeInfo.ratingsCount ?? 0,
-                  // ),
-                ],
-              ),
-            ],
+                ),
+                const SizedBox(
+                  height: 3,
+                ),
+                const Text(
+                  "J.K. Rowling",
+                  style: Styles.textStyle14,
+                ),
+                const SizedBox(
+                  height: 3,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      'Free',
+                      style: Styles.textStyle20.copyWith(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                         const Spacer(),
+                    const BookRating(
+                      rating: 4.8,
+                      count: 245 ,
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ],
       ),
---
+++ lib/features/home/presentation/view/widgets/book_rating.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:iconly/iconly.dart';
+
+import '../../../../../core/utils/styles.dart';
+
+class BookRating extends StatelessWidget {
+  const BookRating({
+    super.key,
+    this.mainAxisAlignment = MainAxisAlignment.start,
+    required this.rating,
+    required this.count,
+  });
+
+  final MainAxisAlignment mainAxisAlignment;
+  final num rating;
+  final int count;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: mainAxisAlignment,
+      children: [
+        const Icon(
+          IconlyBold.star,
+          size: 14,
+          color: Color(0xffFFDD4F),
+        ),
+         SizedBox(
+          width: 6.3.w,
+        ),
+        Text(
+          rating.toString(),
+          style: Styles.textStyle16,
+        ),
+         SizedBox(
+          width: 5.w,
+        ),
+        Opacity(
+          opacity: 0.5,
+          child: Text(
+            '($count)',
+            style: Styles.textStyle14.copyWith(
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
