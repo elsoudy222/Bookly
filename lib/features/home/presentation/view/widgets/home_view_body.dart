5e7b5125c2174e60e64e51beff919b8ecc6c6420
soudyjr <elsoudy222@gmail.com>
2024-01-28 23:48:06 +0400

    Best Seller List View Item Part 3

--- lib/features/home/presentation/view/widgets/home_view_body.dart
+++ lib/features/home/presentation/view/widgets/home_view_body.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../../../../core/utils/const/assets_path/assets_path.dart';
 import '../../../../../core/utils/const/const.dart';
+import 'best_seller_item.dart';
 import 'custom_app_bar.dart';
 import 'featured_list_view.dart';
 
@@ -38,78 +39,4 @@ class HomeViewBody extends StatelessWidget {
   }
 }
 
-class BestSellerListViewItem extends StatelessWidget {
-  const BestSellerListViewItem({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      child: Row(
-        children: [
-          Container(
-            height: 100.h,
-            width: 70.w,
-            decoration:  BoxDecoration(
-              color: Colors.orange,
-              borderRadius: BorderRadius.circular(15),
-              image: const DecorationImage(
-                fit: BoxFit.cover,
-                image: AssetImage(
-                  AssetsPaths.testPic,
-
-                ),
-
-              ),
-            ),
-          ),
-          const SizedBox(
-            width: 30,
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width * .5,
-                child: Text(
-                  "Harry Potter and the Goblet of Fire",
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  style: Styles.textStyle20.copyWith(
-                    fontFamily: kGtSectraFine,
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 3,
-              ),
-              Text(
-                "J.K. Rowling",
-                style: Styles.textStyle14,
-              ),
-              const SizedBox(
-                height: 3,
-              ),
-              Row(
-                children: [
-                  Text(
-                    'Free',
-                    style: Styles.textStyle20.copyWith(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-            //      const Spacer(),
-                  // BookRating(
-                  //   rating:
-                  //   bookModel.volumeInfo.averageRating?.round() ?? 0,
-                  //   count: bookModel.volumeInfo.ratingsCount ?? 0,
-                  // ),
-                ],
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-}
