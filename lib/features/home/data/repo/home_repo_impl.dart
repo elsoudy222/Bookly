90c0ba6fa90664fe7e4df3c2e91f8aa27c126d92
soudyjr <elsoudy222@gmail.com>
2024-02-05 12:23:22 +0400

    create Home Repo Implementation

---
+++ lib/features/home/data/repo/home_repo_impl.dart
@@ -0,0 +1,23 @@
+
+import 'package:bookly/core/errors/failure.dart';
+
+import 'package:bookly/features/home/data/models/book_model.dart';
+
+import 'package:dartz/dartz.dart';
+
+import 'home_repo.dart';
+
+class HomeRepoImpl implements HomeRepo{
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
+    // TODO: implement fetchBestSellerBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
+    // TODO: implement fetchFeaturedBooks
+    throw UnimplementedError();
+  }
+
+}
