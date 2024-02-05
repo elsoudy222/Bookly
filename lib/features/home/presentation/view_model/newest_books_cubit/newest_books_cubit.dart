42429dd0cb7b241ee9e25baee8350849f88bbcd1
soudyjr <elsoudy222@gmail.com>
2024-02-05 22:32:24 +0400

    Create Newest Books Method in cubit

---
+++ lib/features/home/presentation/view_model/newest_books_cubit/newest_books_cubit.dart
@@ -0,0 +1,21 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly/features/home/data/models/book_model.dart';
+import 'package:bookly/features/home/data/repo/home_repo.dart';
+import 'package:equatable/equatable.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+  HomeRepo homeRepo;
+
+  Future<void> getNewestBooks() async {
+    emit(NewestBooksLoading());
+    var result = await homeRepo.fetchNewestBooks();
+    return result.fold((failure) {
+      emit(NewestBooksFailure(failure.errorMessage));
+    }, (books){
+      emit(NewestBooksSuccess(books));
+    });
+  }
+}
