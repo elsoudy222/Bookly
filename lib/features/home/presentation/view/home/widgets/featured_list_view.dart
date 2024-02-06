6851453b00e5509f6beafd96d5cb27c7a04a6921
soudyjr <elsoudy222@gmail.com>
2024-02-06 11:46:56 +0400

    Add Bloc Builder to Featured books

--- lib/features/home/presentation/view/home/widgets/featured_list_view.dart
+++ lib/features/home/presentation/view/home/widgets/featured_list_view.dart
@@ -1,4 +1,6 @@
+import 'package:bookly/features/home/presentation/view_model/featured_books_cubit/featured_book_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'custom_book_item.dart';
@@ -8,18 +10,40 @@ class FeaturedBooksListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 200.h,
-      child: ListView.builder(
-          itemCount: 3,
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, i) {
-            return Padding(
-              padding:  EdgeInsets.only(left: 13.0.w),
-              child: const CustomBookImage(),
-            );
-          }
-      ),
+    return BlocBuilder<FeaturedBookCubit, FeaturedBooksState>(
+      builder: (context, state) {
+         if (state is FeaturedBooksSuccess){
+          return SizedBox(
+            height: 200.h,
+            child: ListView.builder(
+                itemCount: 3,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, i) {
+                  return Padding(
+                    padding: EdgeInsets.only(left: 13.0.w),
+                    child: const CustomBookImage(),
+                  );
+                }
+            ),
+          );
+        }
+         else if(state is FeaturedBooksFailure ){
+          return SizedBox(
+            height: 200.h,
+            child: ListView.builder(
+                itemCount: 3,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, i) {
+                  return Padding(
+                    padding: EdgeInsets.only(left: 13.0.w),
+                    child: const CustomBookImage(),
+                  );
+                }
+            ),
+          );
+        }
+         return const Center(child: CircularProgressIndicator(),);
+      },
     );
   }
 }
