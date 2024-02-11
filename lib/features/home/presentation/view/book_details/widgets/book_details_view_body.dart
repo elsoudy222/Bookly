27ea97a464516eb83bd1f77f9e5f82524b858516
soudyjr <elsoudy222@gmail.com>
2024-02-11 23:19:52 +0400

    edit book details view body

--- lib/features/home/presentation/view/book_details/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/view/book_details/widgets/book_details_view_body.dart
@@ -1,6 +1,8 @@
 import 'package:bookly/features/home/presentation/view/book_details/widgets/similar_books_section.dart';
+import 'package:bookly/features/home/presentation/view_model/featured_books_cubit/featured_book_cubit.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'book_details_section.dart';
 import 'custom_book_details_appbar.dart';
@@ -10,31 +12,42 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        SliverFillRemaining(
-          hasScrollBody: false,
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 25.0.w),
-            child: Column(
-              children: [
-                const CustomBookDetailsAppBar(),
-                ///////////////////////////////////////////////////////////////
-                const BookDetailsSection(),
-                Expanded(
-                  child: SizedBox(
-                    height: 50.h,
+    return BlocBuilder<FeaturedBookCubit, FeaturedBooksState>(
+      builder: (context, state) {
+        if(state is FeaturedBooksSuccess) {
+          return CustomScrollView(
+            slivers: [
+              SliverFillRemaining(
+                hasScrollBody: false,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25.0.w),
+                  child: Column(
+                    children: [
+                      const CustomBookDetailsAppBar(),
+                      ///////////////////////////////////////////////////////////////
+                      BookDetailsSection(bookModel: state.books,),
+                      Expanded(
+                        child: SizedBox(
+                          height: 50.h,
+                        ),
+                      ),
+                      const SimilarBooksSection(),
+                      SizedBox(
+                        height: 26.h,
+                      ),
+                    ],
                   ),
                 ),
-                const SimilarBooksSection(),
-                SizedBox(
-                  height: 26.h,
-                ),
-              ],
-            ),
-          ),
-        )
-      ],
+              )
+            ],
+          );
+        }else if (state is FeaturedBooksFailure){
+          return Center(
+            child: Text(state.errMessage),
+          );
+        }
+        return CircularProgressIndicator()
+      },
     );
   }
 }
